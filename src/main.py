59235e8ccc723fe5c10842124f8966282f05b228
dtesfay2021 <dina.tesfay@mcsaatchi.com>
2023-03-28 16:49:02 +0100

    re-run

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ def main(project, S3_PREFIX):
     overview_slide_index = 3
     mop_slide_index = 4
 
-    # Process each campaign sequentially
+    # Process each campaign sequentialy
     for result in campaign_list:
         campaign = result[0]
 
